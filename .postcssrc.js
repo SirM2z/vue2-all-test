7ed205d11a40b3090fab57ce39b8898d27cab70a
sirm2z <m18367819165@gmail.com>
2017-02-27 17:58:58 +0800

    修改postcs配置

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,8 +3,6 @@
 module.exports = {
   "plugins": {
     // to edit target browsers: use "browserlist" field in package.json
-    "autoprefixer": {
-      browsers: ['last 2 versions']
-    }
+    "autoprefixer": {}
   }
 }
