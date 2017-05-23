c02e2b7434654bd43c42c9b97489f6cf76dd00a6
sirm2z <m18367819165@gmail.com>
2017-05-23 17:34:34 +0800

    update dependencies

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+const index = resolve => require(['@/pages/index'], resolve)
+const test = resolve => require(['@/pages/test'], resolve)
+const test1 = resolve => require(['@/pages/test/test1'], resolve)
+const test2 = resolve => require(['@/pages/test/test2'], resolve)
+
 Vue.use(Router)
 
 export default new Router({
@@ -11,25 +16,25 @@ export default new Router({
       path: '/hello',
       meta: { title: '你好' },
       name: 'hello',
-      component: (resolve) => require(['../pages/index.vue'], resolve)
+      component: index
     },
     {
       path: '/test',
       meta: { title: '测试页面也很霸气' },
       name: 'test',
-      component: (resolve) => require(['../pages/test.vue'], resolve),
+      component: test,
       children: [
         {
           path: 'test1',
           meta: { title: '测试页面1' },
           name: 'test1',
-          component: (resolve) => require(['../pages/test/test1.vue'], resolve)
+          component: test1
         },
         {
           path: 'test2',
           meta: { title: '测试页面2' },
           name: 'test2',
-          component: (resolve) => require(['../pages/test/test2.vue'], resolve)
+          component: test2
         }
       ]
     },
